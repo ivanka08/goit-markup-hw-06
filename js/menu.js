016dd17913b34be0b4629fc48808b26f12cac3e6
ivanka08 <ivankahudic@gmail.com>
2023-07-04 22:26:32 +0300

    Update index.html, styles and menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,16 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-  };
+  const menuContainer = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  const toggleMenu = () => menuContainer.classList.toggle("is-open");
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-  }
+  window
+    .matchMedia("(min-width: 600px)")
+    .addEventListener(
+      "change",
+      e => e.matches && menuContainer.classList.remove("is-open"),
+    );
 })();
